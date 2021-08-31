70925197f5197f1f59a74f68e3415395ecf34ada
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-08-31 10:33:49 +0600

    first commit

---
+++ data-types.js
@@ -0,0 +1,5 @@
+const age = 19;
+const name = 'Shah Jalal';
+const isOld = true;
+
+console.log(typeof isOld);
---
+++ primitive.js
@@ -0,0 +1,13 @@
+/* Data types
+Primitive data types
+1. Number
+2. String
+3. Boolean
+4. Undefined
+5. Null
+6. Symbol
+
+Non-primitive data types
+7. Object
+*/
+
